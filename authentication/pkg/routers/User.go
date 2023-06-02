c8d194a95e4ead1520f007b7490d34e3aab7e31f
SohamRatnaparkhi <soham.ratnaparkhi@gmail.com>
2023-06-02 23:26:10 +0530

    feat: add login router

--- authentication/pkg/routers/User.go
+++ authentication/pkg/routers/User.go
@@ -10,5 +10,6 @@ func SetAuthRouter() chi.Router {
 	var authRouter = chi.NewRouter()
 	authRouter.Get("/", server.HealthCheck)
 	authRouter.Post("/register", users.HandleRegisterUser)
+	authRouter.Post("/login", users.HandleLoginUser)
 	return authRouter
 }
