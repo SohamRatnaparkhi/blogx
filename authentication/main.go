c37c92928a3369a9f1166a6422fc19feaf5ef356
SohamRatnaparkhi <soham.ratnaparkhi@gmail.com>
2023-06-02 18:45:27 +0530

    feat: authentication server setup

---
+++ authentication/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/SohamRatnaparkhi/blogx-backend-go/authentication/pkg/handlers/server"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	godotenv.Load(".env")
+	PORT := os.Getenv("PORT")
+	if PORT == "" {
+		log.Println("No PORT found")
+	}
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
+
+	v1Router := chi.NewRouter()
+	router.Mount("/v1", v1Router)
+	v1Router.Get("/health", server.HealthCheck)
+
+	fmt.Printf("\nAuthentication server starting at http://localhost:%v\n", PORT)
+
+	server := &http.Server{
+		Handler: router,
+		Addr:    ":" + PORT,
+	}
+
+	err := server.ListenAndServe()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Server started at PORT %v", PORT)
+}
---
+++ authentication/pkg/handlers/server/ServerHealth.go
@@ -0,0 +1,16 @@
+package server
+
+import (
+	"net/http"
+
+	"github.com/SohamRatnaparkhi/blogx-backend-go/authentication/pkg/utils"
+)
+
+func HealthCheck(res http.ResponseWriter, _ *http.Request) {
+	type resp struct {
+		Status string `json:"status"`
+	}
+	utils.ResponseJson(res, 200, resp{
+		Status: "ok",
+	})
+}
