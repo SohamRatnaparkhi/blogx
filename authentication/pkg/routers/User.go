e3bb13c91ec92e034032f8af987489fd47a73490
SohamRatnaparkhi <soham.ratnaparkhi@gmail.com>
2023-06-03 00:04:53 +0530

    feat: user logout router

--- authentication/pkg/routers/User.go
+++ authentication/pkg/routers/User.go
@@ -3,6 +3,7 @@ package routers
 import (
 	"github.com/SohamRatnaparkhi/blogx-backend-go/authentication/pkg/handlers/server"
 	users "github.com/SohamRatnaparkhi/blogx-backend-go/authentication/pkg/handlers/user"
+	"github.com/SohamRatnaparkhi/blogx-backend-go/authentication/pkg/middleware"
 	"github.com/go-chi/chi"
 )
 
@@ -11,5 +12,6 @@ func SetAuthRouter() chi.Router {
 	authRouter.Get("/", server.HealthCheck)
 	authRouter.Post("/register", users.HandleRegisterUser)
 	authRouter.Post("/login", users.HandleLoginUser)
+	authRouter.Post("/logout", middleware.Auth(middleware.AuthHandler(users.HandleUserLogout)))
 	return authRouter
 }
