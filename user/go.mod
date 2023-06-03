a0c56a5c8ecbed3a48dc44c64e79fcde2a05326a
SohamRatnaparkhi <soham.ratnaparkhi@gmail.com>
2023-06-03 11:28:20 +0530

    feat: setup basic user server

---
+++ user/go.mod
@@ -0,0 +1,3 @@
+module github.com/SohamRatnaparkhi/blogx-backend-go/user
+
+go 1.20
---
+++ user/pkg/handlers/server/ServerHealth.go
@@ -0,0 +1,23 @@
+package server
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/SohamRatnaparkhi/blogx-backend-go/authentication/pkg"
+	"github.com/SohamRatnaparkhi/blogx-backend-go/authentication/pkg/utils"
+)
+
+func HealthCheck(res http.ResponseWriter, _ *http.Request) {
+	databaseObject := pkg.DbClient
+	if databaseObject == nil {
+		utils.ErrorResponse(res, http.StatusInternalServerError, fmt.Errorf("database error"))
+		return
+	}
+	type resp struct {
+		Status string `json:"status"`
+	}
+	utils.ResponseJson(res, 200, resp{
+		Status: "ok",
+	})
+}
