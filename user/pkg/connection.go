db42974cde0b51a719a893c48b4772e4fe472b15
SohamRatnaparkhi <soham.ratnaparkhi@gmail.com>
2023-06-03 11:33:25 +0530

    feat: database connection

---
+++ user/pkg/connection.go
@@ -0,0 +1,34 @@
+package pkg
+
+import (
+	"database/sql"
+	"log"
+	"os"
+
+	"github.com/SohamRatnaparkhi/blogx-backend-go/db/database"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
+
+func DbInstance() *database.Queries {
+	//database connection
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	db_url := os.Getenv("DB_URL")
+	if db_url == "" {
+		log.Fatal("DB_URL not found in .env file")
+	}
+
+	db, dbErr := sql.Open("postgres", db_url)
+
+	if dbErr != nil {
+		return nil
+	}
+	dbQueries := database.New(db)
+	return dbQueries
+}
+
+var DbClient *database.Queries = DbInstance()
