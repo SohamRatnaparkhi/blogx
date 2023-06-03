9ea52f8c81712a42397306be268bde3894a50d3e
SohamRatnaparkhi <soham.ratnaparkhi@gmail.com>
2023-06-03 14:17:45 +0530

    feat: followers logic sql and go code

---
+++ db/sql/queries/follower.sql
@@ -0,0 +1,15 @@
+-- name: FollowUser :one
+
+INSERT into
+    user_followers (user_id, follower_id)
+VALUES ($1, $2) ON CONFLICT (user_id, follower_id)
+DO NOTHING
+RETURNING *;
+
+-- nqm3: UnfollowUser :one
+
+DELETE FROM user_followers
+WHERE
+    user_id = $1
+    AND follower_id = $2
+RETURNING *;
