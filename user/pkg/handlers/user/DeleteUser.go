d5ad562e93c21c96ac0d134be085c555317e089a
SohamRatnaparkhi <soham.ratnaparkhi@gmail.com>
2023-06-04 13:49:56 +0530

    fix: finally corrected delete user logic:)

--- user/pkg/handlers/user/DeleteUser.go
+++ user/pkg/handlers/user/DeleteUser.go
@@ -2,7 +2,6 @@ package users
 
 import (
 	"net/http"
-	"time"
 
 	"github.com/SohamRatnaparkhi/blogx-backend-go/db/database"
 	"github.com/SohamRatnaparkhi/blogx-backend-go/user/pkg"
@@ -12,20 +11,12 @@ import (
 func DeleteUser(w http.ResponseWriter, req *http.Request, user database.User) {
 	apiConfig := pkg.DbClient
 	user_uuid := user.ID
-	req2, logoutErr := http.NewRequest(http.MethodPost, "http://localhost:8080/v1/api/auth/logout", nil)
-	if logoutErr != nil {
-		utils.ErrorResponse(w, http.StatusInternalServerError, logoutErr)
-		return
-	}
-	req2.Header.Set("Content-Type", "application/json")
-	client := http.Client{
-		Timeout: 10 * time.Second,
-	}
-	_, reqErr := client.Do(req2)
-	if reqErr != nil {
-		utils.ErrorResponse(w, http.StatusInternalServerError, reqErr)
-		return
-	}
+	http.SetCookie(w, &http.Cookie{
+		Name:  "auth_token",
+		Value: "",
+		Path:  "/",
+	})
+
 	err := apiConfig.DeleteUser(req.Context(), user_uuid)
 	if err != nil {
 		utils.ErrorResponse(w, http.StatusInternalServerError, err)
