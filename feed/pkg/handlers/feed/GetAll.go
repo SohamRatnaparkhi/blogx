d35e8bffbe22e882a3ff57a73c22a883a9c919cc
SohamRatnaparkhi <soham.ratnaparkhi@gmail.com>
2023-06-05 00:18:13 +0530

    refractor: package all_posts to feed

--- feed/pkg/handlers/feed/GetAll.go
+++ feed/pkg/handlers/feed/GetAll.go
@@ -1,4 +1,4 @@
-package all_posts
+package feed
 
 import (
 	"net/http"
--- feed/pkg/routers/Feed.go
+++ feed/pkg/routers/Feed.go
@@ -2,7 +2,7 @@ package routers
 
 import (
 	"github.com/SohamRatnaparkhi/blogx-backend-go/authentication/pkg/middleware"
-	"github.com/SohamRatnaparkhi/blogx-backend-go/feed/pkg/handlers/all_posts"
+	"github.com/SohamRatnaparkhi/blogx-backend-go/feed/pkg/handlers/feed"
 	"github.com/SohamRatnaparkhi/blogx-backend-go/feed/pkg/handlers/server"
 	"github.com/go-chi/chi"
 )
@@ -10,8 +10,8 @@ import (
 func SetFeedRouter() chi.Router {
 	var feedRouter = chi.NewRouter()
 	feedRouter.Get("/", server.HealthCheck)
-	feedRouter.Get("/allPosts", middleware.Auth(middleware.AuthHandler(all_posts.AllPostNoPage)))
-	feedRouter.Get("/allPostsPageWise", middleware.Auth(middleware.AuthHandler(all_posts.AllPostWithPage)))
+	feedRouter.Get("/allPosts", middleware.Auth(middleware.AuthHandler(feed.AllPostNoPage)))
+	feedRouter.Get("/allPostsPageWise", middleware.Auth(middleware.AuthHandler(feed.AllPostWithPage)))
 
 	return feedRouter
 }
