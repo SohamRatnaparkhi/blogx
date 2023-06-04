ffd3e6f84e077266e6832acf0177b653237b50f8
SohamRatnaparkhi <soham.ratnaparkhi@gmail.com>
2023-06-04 15:13:49 +0530

    feat: add update, delete post sql

--- db/sql/queries/post.sql
+++ db/sql/queries/post.sql
@@ -2,5 +2,17 @@
 
 INSERT into
     posts (id, user_id, title, body, tags)
-VALUES ($1, $2, $3, $4, $5)
-RETURNING *;
+VALUES ($1, $2, $3, $4, $5) RETURNING *;
+
+-- name: UpdatePost :one
+
+UPDATE posts
+SET
+    title = $2,
+    body = $3,
+    tags = $4
+WHERE id = $1 RETURNING *;
+
+-- name: DeletePost :exec
+
+DELETE FROM posts WHERE id = $1;
