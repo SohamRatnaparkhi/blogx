0632b25abc14dd527d6706906bbba9d837264be4
SohamRatnaparkhi <soham.ratnaparkhi@gmail.com>
2024-04-30 01:00:13 +0530

    feat: working k8s manifests
    Signed-off-by: SohamRatnaparkhi <soham.ratnaparkhi@gmail.com>

---
+++ k8s/updated/deploy.sh
@@ -0,0 +1,6 @@
+kubectl apply -f ./k8s/updated/api_auth_manifest.yaml 
+kubectl apply -f ./k8s/updated/api_feed_manifest.yaml            
+kubectl apply -f ./k8s/updated/redis_manifest.yaml
+kubectl apply -f ./k8s/updated/api_blog_manifest.yaml 
+kubectl apply -f ./k8s/updated/api_user_manifest.yaml
+kubectl apply -f ./k8s/updated/haproxy_manifest.yaml
